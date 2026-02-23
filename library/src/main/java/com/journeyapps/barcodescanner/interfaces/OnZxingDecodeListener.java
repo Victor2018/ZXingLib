670340663b7161f8f5b505f90cb6651ed1818587
zhanghaifeng <zhanghaifeng@taodaji.com.cn>
2026-02-23 15:30:10 +0800

    remove old code

--- library/src/main/java/com/journeyapps/barcodescanner/interfaces/OnZxingDecodeListener.java
+++
@@ -1,7 +0,0 @@
-package com.journeyapps.barcodescanner.interfaces;
-
-import com.journeyapps.barcodescanner.SourceData;
-
-public interface OnZxingDecodeListener {
-    void OnZxingDecode(SourceData sourceData);
-}
